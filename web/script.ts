367effb90c497be3f55e13c2b74ee01d45f40ea0
Milk-Cool <coder.tomsk@gmail.com>
2026-06-11 20:10:52 +0300

    remote control control in web panel

--- web/script.ts
+++ web/script.ts
@@ -4,6 +4,14 @@ const remote = document.querySelector("#remote") as HTMLDivElement; remote.style
 const namel = document.querySelector("#name") as HTMLInputElement;
 const password = document.querySelector("#password") as HTMLInputElement;
 const save = document.querySelector("#save") as HTMLButtonElement;
+const devices = document.querySelector("#devices") as HTMLSelectElement;
+const feed = document.querySelector("#feed") as HTMLButtonElement;
+const poke = document.querySelector("#poke") as HTMLButtonElement;
+const playRps = document.querySelector("#play-rps") as HTMLButtonElement;
+const rps = document.querySelector("#rps") as HTMLDivElement; rps.style.display = "none";
+const rock = document.querySelector("#rock") as HTMLButtonElement;
+const paper = document.querySelector("#paper") as HTMLButtonElement;
+const scissors = document.querySelector("#scissors") as HTMLButtonElement;
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
@@ -11,6 +19,26 @@ const decoder = new TextDecoder();
 let writer: WritableStreamDefaultWriter, reader: ReadableStreamDefaultReader;
 const wint = async (a: number[]) => await writer.write(new Uint8Array(a).buffer);
 const wu8a = async (a: Uint8Array) => await writer.write(a.buffer);
+
+const strmac = (mac: number[]) => mac.map(x => x.toString(16).padStart(2, "0")).join("-");
+const devlist: Record<string, { name: string, xp: number, level: number, saturation: number }> = {};
+let rxbuf = "";
+
+const txact = async (n: number) => {
+    const mac = devices.value.split("-").map(x => parseInt(x, 16));
+    await writer.write(encoder.encode(JSON.stringify({ mac, action: n }) + "\n").buffer);
+};
+feed.addEventListener("click", () => txact(0));
+poke.addEventListener("click", () => txact(1));
+playRps.addEventListener("click", () => {
+    txact(2);
+    rps.style.display = "";
+});
+rock.addEventListener("click", () => txact(3));
+paper.addEventListener("click", () => txact(4));
+scissors.addEventListener("click", () => txact(5));
+
+devices.addEventListener("change", () => rps.style.display = "none");
 
 connect.addEventListener("click", async () => {
     try {
@@ -21,7 +49,34 @@ connect.addEventListener("click", async () => {
         writer = (port.writable as WritableStream).getWriter();
 
         if(confirm("OK to load the remote interface, Cancel to load the config interface")) {
-
+            remote.style.display = "";
+            setInterval(async () => {
+                const r = await reader.read();
+                rxbuf += decoder.decode(r.value as Uint8Array);
+                while(rxbuf.includes("\n")) {
+                    const idx = rxbuf.indexOf("\n");
+                    const jsonStr = rxbuf.slice(0, idx);
+                    rxbuf = rxbuf.slice(idx + 1);
+                    if(!jsonStr.length || jsonStr[0] !== "{") continue;
+                    try {
+                        const json = JSON.parse(jsonStr);
+                        const m = strmac(json.mac);
+                        if(!devlist[m]) {
+                            devlist[m] = { name: "", xp: 0, level: 0, saturation: 0 };
+                            
+                            const el = document.createElement("option");
+                            el.id = m;
+                            el.value = m;
+                            devices.appendChild(el);
+                        }
+                        devlist[m].name = json.name;
+                        devlist[m].xp = json.xp;
+                        devlist[m].level = json.level;
+                        devlist[m].saturation = json.saturation;
+                        document.getElementById(m)!.innerText = `${json.name} | ${json.level}L ${json.xp}XP, ${json.saturation} saturation`;
+                    } catch(_) {}
+                }
+            })
         } else {
             config.style.display = "";
             await wint([0x59, 0x44, 0x54, 0x91, 0xff, 0x21, 0x73, 0x65, 0x74, 0x75, 0x70, 0x5f, 0x77, 0x72, 0x69, 0x74, 0x65, 0x00, 0x01, 0x00]);
